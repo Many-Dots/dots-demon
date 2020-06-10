26c1077cdaf3680e52e9c511963036dbd6a8d082
Many-Dots <66704911+Many-Dots@users.noreply.github.com>
2020-06-10 10:59:40 +1000

    bot.js new

---
+++ bot.js
@@ -0,0 +1,15 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+	console.log('I am ready!');
+});
+
+client.on('message', message => {
+	if (message.content === 'ping') {
+		message.reply('pong');
+       }
+});
+
+// THIS  MUST  BE  THIS  WAY
+client.login(process.env.NzIwMDU3OTM3NzE1Nzg5OTg0.XuAcnQ.CPeFCgpizdSMojSKopIHdNLqPmQ);//BOT_TOKEN is the Client Secret
